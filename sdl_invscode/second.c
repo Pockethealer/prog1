257b2f1e88d28024a74f5958e0f0b741a86ccf6e
Klein <laszlo.m.grob@gmail.com>
2024-10-17 19:30:27 +0200

    added working sdl2 program second.c with compile code

--- sdl_invscode/second.c
+++ sdl_invscode/second.c
@@ -1,15 +1,16 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
-#include <math.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[]) {
-    /* SDL inicializálása és ablak megnyitása */
+
+/* ablak megnyitasa */
+void sdl_init(int szeles, int magas, SDL_Window** pwindow, SDL_Renderer** prenderer) {
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         SDL_Log("Nem indithato az SDL: %s", SDL_GetError());
         exit(1);
     }
-    SDL_Window* window = SDL_CreateWindow("SDL peldaprogram", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 440, 360, 0);
+    SDL_Window* window = SDL_CreateWindow("SDL peldaprogram", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, szeles, magas, 0);
     if (window == NULL) {
         SDL_Log("Nem hozhato letre az ablak: %s", SDL_GetError());
         exit(1);
@@ -21,52 +22,60 @@ int main(int argc, char* argv[]) {
     }
     SDL_RenderClear(renderer);
 
-    /* rajzok */
-    int x, y, r;
-    r = 50;
+    *pwindow = window;
+    *prenderer = renderer;
+}
 
-    /* karika */
-    x = 100;
-    y = 100;
-    circleRGBA(renderer, x, y, r, 255, 0, 0, 255);
-    circleRGBA(renderer, x + r, y, r, 0, 255, 0, 255);
-    circleRGBA(renderer, x + r * cos(3.1415 / 3), y - r * sin(3.1415 / 3), r, 0, 0, 255, 255);
 
-    /* antialias karika */
-    x = 280;
-    y = 100;
-    aacircleRGBA(renderer, x, y, r, 255, 0, 0, 255);
-    aacircleRGBA(renderer, x + r, y, r, 0, 255, 0, 255);
-    aacircleRGBA(renderer, x + r * cos(3.1415 / 3), y - r * sin(3.1415 / 3), r, 0, 0, 255, 255);
+int main(int argc, char* argv[]) {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    sdl_init(350, 200, &window, &renderer);
 
-    /* kitoltott kor */
-    x = 100;
-    y = 280;
-    filledCircleRGBA(renderer, x, y, r, 255, 0, 0, 255);
-    filledCircleRGBA(renderer, x + r, y, r, 0, 255, 0, 255);
-    filledCircleRGBA(renderer, x + r * cos(3.1415 / 3), y - r * sin(3.1415 / 3), r, 0, 0, 255, 255);
+    bool quit = false;
+    bool left = false;
+    bool right = false;
+    bool rajz = true;
+    while (!quit) {
+        if (rajz) {
+            if (left)
+                filledTrigonRGBA(renderer, 50, 100, 150, 50, 150, 150, 0x00, 0xC0, 0x00, 0xFF);
+            else
+                filledTrigonRGBA(renderer, 50, 100, 150, 50, 150, 150, 0xFF, 0x00, 0x00, 0xFF);
+            if (right)
+                filledTrigonRGBA(renderer, 300, 100, 200, 50, 200, 150, 0x00, 0xC0, 0x00, 0xFF);
+            else
+                filledTrigonRGBA(renderer, 300, 100, 200, 50, 200, 150, 0xFF, 0x00, 0x00, 0xFF);
+            SDL_RenderPresent(renderer);
+            rajz = false;
+        }
 
-    /* attetszo kor */
-    x = 280;
-    y = 280;
-    filledCircleRGBA(renderer, x, y, r, 255, 0, 0, 96);
-    filledCircleRGBA(renderer, x + r, y, r, 0, 255, 0, 96);
-    filledCircleRGBA(renderer, x + r * cos(3.1415 / 3), y - r * sin(3.1415 / 3), r, 0, 0, 255, 96);
+        SDL_Event event;
+        SDL_WaitEvent(&event);
 
-    /* szoveg */
-    stringRGBA(renderer, 110, 350, "Kilepeshez: piros x az ablakon", 255, 255, 255, 255);
+        switch (event.type) {
+            /* felhasznaloi esemeny: ilyeneket general az idozito fuggveny */
+        case SDL_KEYUP:
+            switch (event.key.keysym.sym) {
+            case SDLK_LEFT: left = false; rajz = true; break;
+            case SDLK_RIGHT: right = false; rajz = true; break;
+            case SDLK_ESCAPE: quit = true; break;
+            }
+            break;
 
-    /* az elvegzett rajzolasok a kepernyore */
-    SDL_RenderPresent(renderer);
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym) {
+            case SDLK_LEFT: left = true; rajz = true; break;
+            case SDLK_RIGHT: right = true; rajz = true; break;
+            }
+            break;
 
-    /* varunk a kilepesre */
-    SDL_Event ev;
-    while (SDL_WaitEvent(&ev) && ev.type != SDL_QUIT) {
-        /* SDL_RenderPresent(renderer); - MacOS Mojave esetén */
+        case SDL_QUIT:
+            quit = true;
+            break;
+        }
     }
 
-    /* ablak bezarasa */
     SDL_Quit();
-
     return 0;
 }
