bbe0f6e53c6390a51b3088731a7788b05a1b710f
prono <laszlo.m.grob@gmail.com>
2024-10-22 14:35:16 +0200

    szorgalmi_vilagterkep1.1

---
+++ szorgalmi/vilagterkep/vilagterkep.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <../../src/include/SDL2/SDL.h>
+#include <../../src/include/SDL2/SDL2_gfxPrimitives.h>
+
+int main(int argc, int**argv){
+
+    
+}
